0ff2fc70c611826790f24f34c6079f9b88714eda
avinash <avinashgurugubelli@gmail.com>
2021-06-29 08:41:36 +0530

    doc added for api_consume_example.ts

--- node_project/src/api_consume_example.ts
+++ node_project/src/api_consume_example.ts
@@ -4,29 +4,54 @@ import { MockApiModel } from './models/mock-api-model';
 
 export class ApiConsumeExample {
 
+    /**
+     * Get all articles -> it will fetch all the records from the backend. -> returns multiple records
+     */
     public async getAllArticles() {
-        let apiResponse: Promise<AxiosResponse<MockApiModel[]>> = HttpClient.get<MockApiModel[]>("https://60d824306f13520017a68179.mockapi.io/api/v1/articles");
+        const url: string = "https://60d824306f13520017a68179.mockapi.io/api/v1/articles";
+        let apiResponse: Promise<AxiosResponse<MockApiModel[]>> = HttpClient.get<MockApiModel[]>(url);
         console.log((await apiResponse).data);
     }
 
+    /**
+     * it will fire a post request to API -> to create a entry
+     * @param data holds the data to post to backend
+     */
     public async addArticle(data: MockApiModel) {
-        await HttpClient.post("https://60d824306f13520017a68179.mockapi.io/api/v1/articles", data);
+        const url: string = "https://60d824306f13520017a68179.mockapi.io/api/v1/articles";
+        await HttpClient.post(url, data);
         // then print all details
         await this.getAllArticles();
     }
 
+    /**
+     * get the article by ID -> returns single record.
+     * @param id holds the id of an entity to fetch
+     */
     public async getArticle(id: number) {
-        let apiResponse: Promise<AxiosResponse<MockApiModel[]>> = HttpClient.get<MockApiModel[]>("https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id);
+        const url: string = "https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id;
+        let apiResponse: Promise<AxiosResponse<MockApiModel>> = HttpClient.get<MockApiModel>(url);
         console.log((await apiResponse).data);
     }
 
+    /**
+     * fires a delete request - to delete entity
+     * @param id holds the ID of an entity to delete from backend
+     */
     public async deleteArticle(id: number) {
-        let apiResponse: Promise<AxiosResponse<MockApiModel[]>> = HttpClient.delete<MockApiModel[]>("https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id);
+        const url: string = "https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id;
+        let apiResponse: Promise<AxiosResponse<MockApiModel>> = HttpClient.delete<MockApiModel>(url);
         console.log((await apiResponse).data);
     }
 
+    /**
+     * fires PUT req -> and updates the details with the ID.
+     * @param id holds the id of the entity to update in the backend.
+     * @param data holds the data to update.
+     */
     public async updateArticle(id: number, data: MockApiModel) {
-        let apiResponse: Promise<AxiosResponse<MockApiModel>> = HttpClient.put<MockApiModel>("https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id, data);
+        const url: string = "https://60d824306f13520017a68179.mockapi.io/api/v1/articles/" + id;
+        let apiResponse: Promise<AxiosResponse<MockApiModel>> = HttpClient.put<MockApiModel>(url, data);
         console.log((await apiResponse).data);
     }
 }
@@ -43,4 +68,4 @@ api.updateArticle(51, {
     name: 'Avinash Gurugubelli',
     avatar: 'https://cdn.fakercloud.com/avatars/yecidsm_128.jpg',
     id: '51'
-})
+});
--- node_project/src/util/http-client.ts
+++ node_project/src/util/http-client.ts
@@ -2,8 +2,8 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export class HttpClient {
 
-    public static get<T>(url: string): Promise<AxiosResponse<T>> {
-        return axios.get<T>(url);
+    public static get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return axios.get<T>(url, config);
     }
 
     public static post<T, R = T>(url: string, data: T, config?: AxiosRequestConfig): Promise<AxiosResponse<R>> {
@@ -18,3 +18,24 @@ export class HttpClient {
         return axios.delete<R>(url, config);
     }
 }
+
+// class customHttpClient {
+//     public static get<T>(url: string, config: CustomHttpParams): Promise<AxiosResponse<T>> {
+//         const convertedConfig: AxiosRequestConfig = {
+//             auth: {
+//                 username: config.u_userName,
+//                 password: config.p_password
+//             },
+//             headers: config.h_headers
+//         };
+//         return axios.get<T>(url, convertedConfig);
+//     }
+// }
+
+// interface CustomHttpParams {
+//     u_userName: string;
+//     p_password: string;
+//     h_headers: {
+//         [key: string]: string;
+//     }
+// }
